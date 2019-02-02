d0f90b8c2c20c4d9c93aae6a0cfcc86c1dd83de0
Amanda <amanda.waller.33@gmail.com>
2019-02-01 21:21:39 -0500

    added express/morgan/mongoose/.config/.models/json

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
+"use strict";
+
 const express = require("express");
 // we'll use morgan to log the HTTP layer
 const morgan = require("morgan");
+const mongoose = require('mongoose');
+// Mongoose internally uses a promise-like object,
+// but it's better to make Mongoose use built in es6 promises
+mongoose.Promise = global.Promise;
+// config.js is where we control constants for entire
+// app like PORT and DATABASE_URL
+const {PORT, DATABASE_URL} = require('./config');
+const {blogPosts} = require('./models');
 // we'll use body-parser's json() method to 
 // parse JSON data sent in requests to this app
 // "The first detail to note is that we're using body-parser's
@@ -9,15 +19,16 @@ const bodyParser = require("body-parser");
 // "jsonParser is a piece of middleware that we supply as a
 // second argument to our route handler below."
 const jsonParser = bodyParser.json();
-
-// we import the ShoppingList model, which we'll
-// interact with in our GET endpoint
-const blogPostsRouter = require("./blogPostsRouter");
 const app = express();
 
 // log the http layer
 app.use(morgan('common'));
 app.use(express.json());
+
+
+
+
+
 
 // app.use routes requests to /blogPostsRouter
 // to the blogPostsRouter const
